02000e7cda2f2a9daf7a389cb25b2e66932b7d2c
Logan Ward <ward.logan.t@gmail.com>
2014-06-20 13:13:44 -0500

    Implemented PropertyFormulaRanker

--- src/magpie/optimization/rankers/PropertyFormulaRanker.java
+++ src/magpie/optimization/rankers/PropertyFormulaRanker.java
@@ -9,26 +9,121 @@ package magpie.optimization.rankers;
 import java.util.List;
 import magpie.data.BaseEntry;
 import magpie.data.MultiPropertyEntry;
+import expr.*;
+import java.util.LinkedList;
+import magpie.data.MultiPropertyDataset;
 
 /**
  * Use a formula of several properties. Requires that entries are instances of
  *  {@linkplain MultiPropertyEntry}.
+ * 
+ * <usage><p><b>Usage</b>: $&lt;data template&gt; &lt;formula...&gt;
+ * <pr><br><i>data template</i>: Template used to determine whether properties exist in dataset
+ * <pr><br><i>formula</i>: Formula to use as objective function. Property names must be surrounded by #{}'s.
+ * <br>Example formula: #{volume_pa} * #{bandgap}</usage>
+ * 
  * @author Logan Ward
  */
 public class PropertyFormulaRanker extends EntryRanker {
-
+    /** Engine used to evaluate formula */
+    private Expr Evaluator = null;
+    /** Holds links to the value of variables in the Expr formula */
+    final private List<Variable> Variables = new LinkedList<>();
+    /** Index of properties used in the formula */
+    private int[] PropertyIndex;
+    
     @Override
     public void setOptions(List<Object> Options) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String formula;
+        MultiPropertyDataset template;
+        try {
+            template = (MultiPropertyDataset) Options.get(0);
+            formula = Options.get(1).toString();
+        } catch (Exception e) {
+            throw new Exception(printUsage());
+        }
+        setFormula(template, formula);
     }
     
     @Override
     public String printUsage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Usage: $<data template> <formula...>";
+    }
+    
+    /**
+     * Define the formula used by this class. Names of properties used in the 
+     *  calculation should be surrounded #{}'s.
+     * @param formula Formula to be used
+     * @throws Exception If parsing failed or template does not contain 
+     */
+    public void setFormula(MultiPropertyDataset template, String formula) throws Exception {
+        // Find variables in the formula
+        formula = extractVariables(formula);
+        // Ensure dataset contains those variables
+        PropertyIndex = new int[Variables.size()];
+        for (int i=0; i < Variables.size(); i++) {
+            PropertyIndex[i] = template.getPropertyIndex(Variables.get(i).name());
+            if (PropertyIndex[i] == -1) {
+                throw new Exception("Dataset does not contain property: " + Variables.get(i).name());
+            }
+        }
+        
+        // Generate the expression
+        Parser Parser = new Parser();
+        // Define the variable set
+        for (Variable var : Variables) {
+            Parser.allow(var);
+        }
+        // Parse the expression
+        Evaluator = Parser.parseString(formula);
+    }
+    
+    /**
+     * Parse a formula to find names of properties used as variables. They should
+     *  be surrounded by #{}'s. Stores result internally.
+     * @param Formula Formula to be parsed
+     * @return Formula in a form parsable by Expr
+     */
+    protected String extractVariables(String Formula) throws Exception {
+        // Prepare 
+        Variables.clear();
+        List<String> variableNames = new LinkedList<>();
+        
+        // Parse formula
+        int curPos = 0; // Current position of parser
+        while (true) {
+            // Find the next variable
+            int varPos = Formula.indexOf("#{", curPos);
+            if (varPos == -1) break; // No more variables
+            int varEnd = Formula.indexOf("}", varPos);
+            if (varEnd == -1) throw new Exception("Poorly formed expression - Can't find }");
+            curPos = varEnd; // Move up the serach
+            String varName = Formula.substring(varPos + 2, varEnd);
+            variableNames.add(varName);
+        }
+        
+        // Create a list of variables with the following order
+        // Also, remove the #{<...>} notation from the formula string
+        for (String varName : variableNames) {
+            Variable newVariable = Variable.make(varName);
+            Formula = Formula.replace("#{" + varName + "}", varName);
+            Variables.add(newVariable);
+        }
+        
+        return Formula;
     }
     
     @Override
     public double objectiveFunction(BaseEntry Entry) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (! (Entry instanceof MultiPropertyEntry)) {
+            throw new Error("Entry must be a MultiPropertyEntry");
+        }
+        MultiPropertyEntry e = (MultiPropertyEntry) Entry;
+        for (int i=0; i<Variables.size(); i++) {
+            double x = UseMeasured ? e.getMeasuredProperty(PropertyIndex[i]) 
+                : e.getMeasuredProperty(PropertyIndex[i]);
+            Variables.get(i).setValue(x);
+        }
+        return Evaluator.value();
     }
 }
