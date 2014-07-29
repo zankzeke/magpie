3551eea3b133e878b3c099c539e7f62bbca4e7a7
Logan Ward <ward.logan.t@gmail.com>
2014-07-29 09:41:47 -0500

    Upgraded ErrorSignalModifier to produce either absolute or relative error

--- src/magpie/data/utilities/modifiers/ErrorSignalModifier.java
+++ src/magpie/data/utilities/modifiers/ErrorSignalModifier.java
@@ -13,8 +13,10 @@ import magpie.models.BaseModel;
  * Subtract the predicted class from a dataset, leaving only the error signal as the 
  *  measured class variable. Only works with regression models.
  * 
- * <usage><p><b>Usage</b>: $&lt;model>
- * <br><pr><i>model</i>: Model to use to generate ErrorSignal</usage>
+ * <usage><p><b>Usage</b>: $&lt;model&gt; &lt;absolute|relative&gt;
+ * <br><pr><i>model</i>: Model to use to generate error signal
+ * <br><pr><i>absolute|relative</i>: Whether to use absolute or relative error
+ * </usage>
  * 
  * @author Logan Ward
  * @version 0.1
@@ -22,11 +24,21 @@ import magpie.models.BaseModel;
 public class ErrorSignalModifier extends BaseDatasetModifier {
     /** Model used to make class predictions */
     private BaseModel Model = null;
+    /** Whether to calculate absolute error */
+    private boolean ComputeAbsolute = true;
 
     @Override
     public void setOptions(List Options) throws Exception {
         try {
             setModel((BaseModel) Options.get(0));
+            String word = Options.get(1).toString().toLowerCase();
+            if (word.startsWith("abs")) {
+                useAbsoluteError();
+            } else if (word.startsWith("rel")) {
+				useRelativeError();
+			} else {
+				throw new Exception();
+			}
         } catch (Exception e) {
             throw new Exception(printUsage());
         }
@@ -34,7 +46,21 @@ public class ErrorSignalModifier extends BaseDatasetModifier {
 
     @Override
     public String printUsage() {
-        return "Usage: $<model>";
+        return "Usage: $<model> <absolute|relative>";
+    }
+    
+    /**
+     * Set filter to compute absolute error.
+     */
+    public void useAbsoluteError() {
+        ComputeAbsolute = true;
+    }
+    
+    /**
+     * Set filter to computer relative error.
+     */
+    public void useRelativeError() {
+        ComputeAbsolute = false;
     }
 
     /**
@@ -66,9 +92,14 @@ public class ErrorSignalModifier extends BaseDatasetModifier {
         Model.run(Data);
         double[] predicted = Data.getPredictedClassArray();
         Data.setPredictedClasses(new double[Data.NEntries()]); // Sets predicted class to all 0's
-        for (int i=0; i<measured.length; i++)
-            measured[i] -= predicted[i];
-        Data.setMeasuredClasses(measured);
+		double[] error = new double[measured.length];
+        for (int i=0; i<measured.length; i++) {
+            error[i] = measured[i] - predicted[i];
+			if (! ComputeAbsolute) {
+				error[i] /= measured[i];
+			}
+		}
+        Data.setMeasuredClasses(error);
     }
 
 }
