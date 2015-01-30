a8780ff4eacf7700e961209d100599c64fde3a76
Logan Ward <ward.logan.t@gmail.com>
2015-01-30 13:03:47 -0600

    Added tests for DMSPEngine (and also some for the base class)

---
+++ tests/magpie/csp/DMSPEngineTest.java
@@ -0,0 +1,41 @@
+package magpie.csp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for the DMSPEngine. Also includes some tests for the base class
+ * @author Logan Ward
+ */
+public class DMSPEngineTest {
+
+    @Test
+    public void testImportCompounds() throws Exception {
+        DMSPEngine csp = new DMSPEngine();
+        csp.importKnownCompounds("datasets/prototypes.list");
+    }
+    
+    @Test
+    public void testStructurePrediction() throws Exception {
+        // Initilize the engine
+        DMSPEngine csp = new DMSPEngine();
+        csp.importKnownCompounds("datasets/prototypes.list");
+        
+        // Compute structure for Na5Pb2
+        csp.predictStructure("Na5Pb2");
+    }
+    
+    @Test
+    public void testCrossValidation() throws Exception {
+        // Initilize the engine
+        DMSPEngine csp = new DMSPEngine();
+        csp.importKnownCompounds("datasets/prototypes.list");
+        
+        // Cross-validate for binaries
+        csp.crossvalidate(2, 10);
+        
+        // LOOCV for elements
+        csp.crossvalidate(1, 0);
+    }
+    
+}
