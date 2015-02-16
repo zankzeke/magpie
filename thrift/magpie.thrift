b54ddfb3262df8c94afb213610a461176dd3ddcb
Logan Ward <ward.logan.t@gmail.com>
2015-02-16 14:15:56 -0600

    Finished implementing the "evaluateProperties()" command

--- thrift/magpie.thrift
+++ thrift/magpie.thrift
@@ -1,18 +1,16 @@
-struct entry {
+struct Entry {
 	1: string name
 	2: map<string,double> measured_properties = {}
 }
 
-exception MagpieServerException {
-	1: string message
-}
-
 service MagpieServer {
 	/**
-         * Compute the properties of each entry in a list
-         * @param entries [in] List of entries to be evaluated
+	 * Compute the properties of each entry in a list. Returns results formatted as string
+	 * in a human-readable format.
+	 * @param entries [in] List of entries to be evaluated
          * @param props [in] Names of properties to evaluate
-	 * @return List of each property for each entry
+	 * @return List of each property for each entry (in string format)
 	 */
-	list<list<double>> evaluateProperties(1:list<entry> entries, 2:list<string> props) throws (1:MagpieServerException ouch)
+	list<list<string>> evaluateProperties(1:list<Entry> entries, 2:list<string> props)
+	
 }
