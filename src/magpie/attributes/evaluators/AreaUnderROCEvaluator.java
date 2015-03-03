3667d311cc4b2bd803f672f2bc8e1fe7b863bcce
Logan Ward <ward.logan.t@gmail.com>
2015-03-03 13:08:29 -0600

    Upgraded AreaUnderROCEvaluator to handle more than 2 classes

--- src/magpie/attributes/evaluators/AreaUnderROCEvaluator.java
+++ src/magpie/attributes/evaluators/AreaUnderROCEvaluator.java
@@ -4,31 +4,49 @@ package magpie.attributes.evaluators;
 import java.util.List;
 import magpie.data.Dataset;
 import magpie.optimization.algorithms.OptimizationHelper;
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Ranks entries based on the area under the <a href="http://en.wikipedia.org/wiki/Receiver_operating_characteristic">
  * Receiver Operating Characteristic Curve</a>. This measure is best used when 
  * testing the ability of attributes to partition data between two different 
- * classes. In fact, it can only be used if the dataset in question has two exactly classes.
+ * classes. In the event that more than one class is present, 
+ * it will report the ability to distinguish whether an entry is a certain, 
+ * user-specified class.
  * 
- * <usage><p><b>Usage</b>: *No options to set*</usage>
+ * 
+ * <usage><p><b>Usage</b>: [&lt;class&gt;]
+ * <pr><br><i>class</i>: Name of class for which the ability to distinguish is being assessed (default: class #0)</usage>
+ * 
  * @author Logan Ward
  * @version 0.1
  */
 public class AreaUnderROCEvaluator extends BaseAttributeEvaluator {
+    /** Class for which the ability to distinguish is being assesed */
+    private String TargetClass = null;
 
     @Override
     public void setOptions(List<Object> Options) throws Exception {
-        /* Nothing to set */
+        if (Options.size() == 1) {
+            
+        } else if (! Options.isEmpty()) {
+            throw new Exception(printUsage());
+        }
     }
 
     @Override
     public String printUsage() {
-        return "Usage: *No options*";
+        return "Usage: <class>";
+    }
+
+    /**
+     * Define the class for which the ability to classify is being assessed.
+     * @param className Name of class
+     */
+    public void setTargetClass(String className) {
+        this.TargetClass = className;
     }
     
-    
-
     @Override
     protected boolean positiveIsBetter() {
         return true;
@@ -36,8 +54,17 @@ public class AreaUnderROCEvaluator extends BaseAttributeEvaluator {
 
     @Override
     protected double[] evaluateAttributes_internal(Dataset Data) {
-        if (Data.NClasses() != 2)
-            throw new Error("Dataset must have exactly two classes");
+        if (Data.NClasses() == 1) {
+            throw new Error("Class variable must have multiple classes");
+        }
+        // Get class ID
+        int classID = 0;
+        if (TargetClass != null) {
+            classID = ArrayUtils.indexOf(Data.getClassNames(), TargetClass);
+            if (classID == -1) {
+                throw new Error("No such class: " + TargetClass);
+            }
+        }
         
         // Allocate needed arrays
         double[] class_array = Data.getMeasuredClassArray();
@@ -50,11 +77,11 @@ public class AreaUnderROCEvaluator extends BaseAttributeEvaluator {
             int[] rank = OptimizationHelper.sortAndGetRanks(feature, true);
             double auc=0; int type0found=0;
             for (int j=0; j<Data.NEntries(); j++) 
-                if (class_array[rank[j]] == 0) 
+                if (class_array[rank[j]] == classID) 
                     type0found++;
                 else
                     auc+=type0found;
-            auc = auc / dist[0] / dist[1] - 0.5;
+            auc = auc / dist[classID] / (class_array.length - dist[classID]) - 0.5;
             auc = Math.abs(auc) + 0.5;
             output[i] = auc;
         }
