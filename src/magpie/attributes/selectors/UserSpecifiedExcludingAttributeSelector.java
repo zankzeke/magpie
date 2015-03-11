4b7b82455db0fc2c2a3d7b5aa8785acb9be42e0e
Logan Ward <ward.logan.t@gmail.com>
2015-03-11 16:16:46 -0500

    Added attribute selector design to exclude certain attributes

---
+++ src/magpie/attributes/selectors/UserSpecifiedExcludingAttributeSelector.java
@@ -0,0 +1,50 @@
+package magpie.attributes.selectors;
+
+import java.util.LinkedList;
+import java.util.List;
+import magpie.data.Dataset;
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * Exclude attributes with a certain name
+ * 
+ * <usage><p><b>Usage</b>: &lt;names...>
+ * <br><pr><i>names</i>: Names of attributes to select</usage>
+ * 
+ * @author Logan Ward
+ */
+public class UserSpecifiedExcludingAttributeSelector extends 
+        UserSpecifiedAttributeSelector {
+
+    /**
+     * Define list of attributes that will be excluded before training a model.
+     * @param Attributes List of attribute names
+     */
+    @Override
+    public void selectAttributes(List<String> Attributes) {
+        super.selectAttributes(Attributes); 
+    }
+
+    @Override
+    protected List<Integer> train_protected(Dataset Data) {
+        List<Integer> output = new LinkedList<>();
+        String[] Names = Data.getAttributeNames();
+        
+        // Prepare a list of attributes to exclude
+        for (int i=0; i<Data.NAttributes(); i++) {
+            output.add(i);
+        }
+        
+        // Exclude those attributes
+        for (String name : SelectedAttributes) {
+            int id = ArrayUtils.indexOf(Names, name);
+            if (id == ArrayUtils.INDEX_NOT_FOUND) {
+                throw new Error("Attribute \""+name+"\" not found");
+            }
+            output.remove(id);
+        }
+        return output;
+    }
+    
+    
+}
---
+++ tests/magpie/attributes/selectors/UserSpecifiedExcludingAttributeSelectorTest.java
@@ -0,0 +1,37 @@
+package magpie.attributes.selectors;
+
+import java.util.*;
+import magpie.data.Dataset;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Logan Ward
+ */
+public class UserSpecifiedExcludingAttributeSelectorTest {
+
+    @Test
+    public void test() throws Exception {
+        // Load in dataset
+        Dataset data = new Dataset();
+        data.importText("datasets/simple-data.csv", null);
+        
+        // Create attribue selector
+        UserSpecifiedExcludingAttributeSelector selector = 
+                new UserSpecifiedExcludingAttributeSelector();
+        List<String> attrs = new LinkedList<>();
+        attrs.add("x");
+        selector.selectAttributes(attrs);
+        
+        
+        // Run it
+        selector.train(data);
+        selector.run(data);
+        
+        // Test result
+        assertEquals(1, data.NAttributes());
+        assertEquals("y", data.getAttributeNames()[0]);
+    }
+    
+}
