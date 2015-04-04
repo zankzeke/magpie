3ebd982defbc8bd324387bd2d1fc6cc812da0100
Logan Ward <ward.logan.t@gmail.com>
2015-04-03 21:10:34 -0500

    Bug fix: Improved math in linear correction term

--- tests/magpie/models/regression/LinearCorrectedRegressionTest.java
+++ tests/magpie/models/regression/LinearCorrectedRegressionTest.java
@@ -1,6 +1,10 @@
 
 package magpie.models.regression;
 
+import magpie.analytics.RegressionStatistics;
+import magpie.data.Dataset;
+import magpie.data.materials.CompositionDataset;
+import magpie.data.utilities.splitters.MixedMetalsSplitter;
 import magpie.models.BaseModel;
 import magpie.models.BaseModelTest;
 import org.junit.Test;
@@ -14,7 +18,7 @@ public class LinearCorrectedRegressionTest extends BaseModelTest {
 
     @Override
     public BaseModel generateModel() {
-        BaseModel sub = new WekaRegression();
+        BaseModel sub = new PolynomialRegression();
         LinearCorrectedRegression model = new LinearCorrectedRegression();
         try {
             model.setSubmodel(sub);
@@ -22,6 +26,30 @@ public class LinearCorrectedRegressionTest extends BaseModelTest {
             throw new Error(e);
         }
         return model;
+    }
+    
+    @Test
+    public void testImprovement() throws Exception {
+        CompositionDataset data = new CompositionDataset();
+        data.importText("datasets/small_set.txt", null);
+        data.generateAttributes();
+        data.setTargetProperty("delta_e", true);
+        
+        SplitRegression sub = new SplitRegression();
+        sub.setPartitioner(new MixedMetalsSplitter());
+        sub.setGenericModel(new WekaRegression("trees.REPTree", null));
+        sub.train(data);
+        
+        LinearCorrectedRegression model = new LinearCorrectedRegression();
+        model.setSubmodel(sub);
+        model.train(data);
+        
+        // Get MAE
+        RegressionStatistics s1 = (RegressionStatistics) sub.TrainingStats;
+        RegressionStatistics s2 = (RegressionStatistics) model.TrainingStats;
+        s1.savePerformanceData("data_uncorrected.csv");
+        s2.savePerformanceData("data_corrected.csv");
+        assertTrue(s1.MAE - s2.MAE > -1e-6);
     }
     
 }
