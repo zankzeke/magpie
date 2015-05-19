478a00aac32d372183fd6bde61a30118f1c4bc34
Logan Ward <ward.logan.t@gmail.com>
2015-05-19 14:52:34 -0500

    Started to implement attributes specific to ionic compounds

--- src/magpie/attributes/generators/IonicCompoundAttributeGenerator.java
+++ src/magpie/attributes/generators/IonicCompoundAttributeGenerator.java
@@ -31,6 +31,9 @@ public class IonicCompoundAttributeGenerator extends BaseAttributeGenerator {
 
     @Override
     public void addAttributes(Dataset data) {
+        if (! (data instanceof CompositionDataset)) {
+            throw new Error("Data must implement composition dataset");
+        }
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
