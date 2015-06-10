e7eaacbeabb060e83e09507aea7178410bcbfe90
Logan Ward <ward.logan.t@gmail.com>
2015-06-10 09:37:23 -0500

    Implemented model compression for sklearn server demo

--- py/scikit_client.py
+++ py/scikit_client.py
@@ -2,6 +2,7 @@ import socket
 from sys import argv
 import pickle
 from numpy import mean
+import bz2
 
 port = int(argv[1])
 
@@ -17,7 +18,7 @@ def connect(port):
 s, fo, fi = connect(port)
 
 # Make fake dataset
-X = [[x, 1/(x+1), x+4, x*x] for x in range(10)]
+X = [[x, 1/(x+1), x+4, x*x] for x in range(100)]
 y = [x[0] * 2 + x[0] * x[0] for x in X]
 
 # Send in training data
@@ -28,8 +29,22 @@ for xi,yi in zip(X,y):
 	print >>fo, yi, " ".join([str(xij) for xij in xi])
 fo.flush()
 
-# Get the model back
-model = pickle.load(fi)
+# Receive model
+model_compressed = fi.read()
+print "Compressed size:", len(model_compressed)
+model_string = bz2.decompress(model_compressed)
+print "Decompressed size:", len(model_string)
+
+# Save the compressed model (debugging purposes)
+fp = open('model.compressed', 'w')
+import binascii
+print >>fp, binascii.hexlify(model_compressed)
+fp.close()
+
+# Unpickle the model
+model = pickle.loads(model_string);
+model_compressed = None
+model_string = None
 print model
 
 # Close up
