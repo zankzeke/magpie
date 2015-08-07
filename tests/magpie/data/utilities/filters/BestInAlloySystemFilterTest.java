06f6c31e5d2a7883006208a2d98ffbec60f4dcd5
Logan Ward <ward.logan.t@gmail.com>
2015-08-07 13:35:48 -0500

    Added tool to only select only the best entries in each alloy system

---
+++ tests/magpie/data/utilities/filters/BestInAlloySystemFilterTest.java
@@ -0,0 +1,44 @@
+package magpie.data.utilities.filters;
+
+import java.util.LinkedList;
+import java.util.List;
+import magpie.data.materials.CompositionDataset;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Logan Ward
+ */
+public class BestInAlloySystemFilterTest {
+
+    @Test
+    public void test() throws Exception {
+        // Make test dataset
+        CompositionDataset data = new CompositionDataset();
+        data.addEntry("NaTi");
+        data.getEntry(0).setMeasuredClass(1.0);
+        data.addEntry("NaTi2");
+        data.getEntry(1).setMeasuredClass(2.0);
+        data.addEntry("NaTi2Fe");
+        data.getEntry(2).setMeasuredClass(1.0);
+        
+        // Make filter
+        BestInAlloySystemFilter filter = new BestInAlloySystemFilter();
+        List<Object> options = new LinkedList<>();
+        options.add("1");
+        options.add("maximize");
+        options.add("measured");
+        options.add("TargetEntryRanker");
+        options.add("1.0");
+        filter.setOptions(options);
+        filter.setExclude(false);
+        
+        // Run filter
+        boolean[] res = filter.label(data);
+        assertFalse(res[0]);
+        assertTrue(res[1]);
+        assertTrue(res[2]);
+    }
+    
+}
