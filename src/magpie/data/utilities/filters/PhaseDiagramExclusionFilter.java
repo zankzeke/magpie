f69c1d02b5c72f835649fc5da3571ddf595f3c3c
Logan Ward <ward.logan.t@gmail.com>
2015-09-22 11:43:47 -0500

    Modified behavior of PhaseDiagramExclusionFilter
    Now filters out entries that contain all of the elements in the phase diagram being considered

--- src/magpie/data/utilities/filters/PhaseDiagramExclusionFilter.java
+++ src/magpie/data/utilities/filters/PhaseDiagramExclusionFilter.java
@@ -9,9 +9,16 @@ import magpie.user.CommandHandler;
 import org.apache.commons.lang3.ArrayUtils;
 
 /**
- * Filter entries that contain only certain elements. Only works on {@link CompositionDataset}.
- * Consider this a way to remove any entries from a certain phase diagram from 
- * a dataset.
+ * Filter entries from certain phase diagrams. Removes entries that only contain
+ * elements from a certain set and those that contain all elements from that set.
+ * 
+ * <p>Example: When excluding entries from the Al-Ni phase diagram
+ * <ol>
+ * <li>Al will be removed because it contains Al and no other element
+ * <li>AlNi will be removed because it contains both Al and Ni
+ * <li>AlZr will not be removed because it contains another element
+ * <li>AlNiZr will be removed because it contains both Al and Ni
+ * </ol>
  * 
  * <usage><p><b>Usage</b>: &lt;Element names...>
  * <br><pr><i>Element names...</i>: Entries containing these elements pass the filter</usage>
@@ -28,18 +35,30 @@ public class PhaseDiagramExclusionFilter extends ContainsElementFilter {
         CompositionDataset Data = (CompositionDataset) D;
         int[] ExcludedIndex = getElementIndices(ElementList);
         
-        // Find entries that contain one of those elements
-        boolean[] containsOnly = new boolean[D.NEntries()];
+        boolean[] inDiagram = new boolean[D.NEntries()];
         for (int i=0; i<D.NEntries(); i++) {
-            containsOnly[i] = true;
+            inDiagram[i] = true;
             int[] elems = Data.getEntry(i).getElements();
-            for (int elem : elems) {
-				if (! ArrayUtils.contains(ExcludedIndex, elem)) {
-					containsOnly[i] = false;
-					break;
-				}
-			}
+            
+            if (elems.length <= ExcludedIndex.length) {
+                // Check if this entry contains only elements from the 
+                //  phase diagram in consideration
+                for (int elem : elems) {
+                    if (! ArrayUtils.contains(ExcludedIndex, elem)) {
+                        inDiagram[i] = false;
+                        break;
+                    }
+                }
+            } else {
+                // Check if this entry contains all of the elements in the diagram
+                for (int elem : ExcludedIndex) {
+                    if (! ArrayUtils.contains(elems, elem)) {
+                        inDiagram[i] = false;
+                        break;
+                    }
+                }
+            }
         }
-        return containsOnly;
+        return inDiagram;
 	}
 }
