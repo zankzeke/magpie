3e71ef39ff9b8e2c27a0f75cc3903fd463ea250f
Logan Ward <ward.logan.t@gmail.com>
2016-01-21 08:53:41 -0600

    Renamed BaseDatasetNormalizerTest to a test that only runs the commands

--- src/test/java/magpie/data/utilities/normalizers/BaseDatasetNormalizerCommandsTest.java
+++ src/test/java/magpie/data/utilities/normalizers/BaseDatasetNormalizerCommandsTest.java
@@ -8,10 +8,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Test the command line functions for {@linkplain BaseDatasetNormalizer}.
  * @author Logan Ward
  */
-public class BaseDatasetNormalizerTest {
+public class BaseDatasetNormalizerCommandsTest {
 
     @Test
     public void testCommands() throws Exception {
