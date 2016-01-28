813ca76ea26c36868aa68d464f98ca2dc8fe7f06
Logan Ward <ward.logan.t@gmail.com>
2016-01-28 08:32:06 -0600

    Finished APRDFAttributeGenerator

--- src/main/java/magpie/attributes/generators/crystal/APRDFAttributeGenerator.java
+++ src/main/java/magpie/attributes/generators/crystal/APRDFAttributeGenerator.java
@@ -1,23 +1,30 @@
 package magpie.attributes.generators.crystal;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import magpie.attributes.generators.BaseAttributeGenerator;
+import magpie.data.BaseEntry;
 import magpie.data.Dataset;
+import magpie.data.materials.AtomicStructureEntry;
+import magpie.data.materials.CrystalStructureDataset;
+import magpie.data.materials.util.LookupData;
 import magpie.utility.interfaces.Citable;
 import magpie.utility.interfaces.Citation;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import vassal.analysis.APRDFAnalysis;
+import vassal.data.Cell;
 
 /**
  * Attributes based on the Atomic Property Weighted Radial Distribution Function
- * (AP-RDF) approach of <a href="http://dx.doi.org/10.1021/jz501331m">
+ * (AP-RDF) approach of <a href="http://pubs.acs.org/doi/abs/10.1021/jp404287t">
  * Fernandez <i>et al.</i></a>. 
  * 
  * <p>User can specify the cutoff distance for the AP-RDF, the number of points
- * to evaluate it, the smoothing factors, and the properties used for weighting.
- * The recommended values of these parameters have yet to be determined, please
- * contact Logan Ward, or the authors of this paper if you have questions.
+ * to evaluate it, the smoothing factors for the RDF peaks, and the properties used 
+ * for weighting. The recommended values of these parameters have yet to be determined, please
+ * contact Logan Ward or the authors of this paper if you have questions or ideas
+ * for these parameters.
  * 
  * <usage><p><b>Usage</b>: &lt;cutoff&gt; &lt;# points&gt; &lt;smoothing&gt;
  * &lt;properties&gt;
@@ -30,25 +37,169 @@ import org.apache.commons.lang3.tuple.Pair;
  */
 public class APRDFAttributeGenerator extends BaseAttributeGenerator 
         implements Citable {
+    /** Cutoff distance for RDF */
+    protected double CutoffDistance = 10.0;
+    /** Number of points to evaluate */
+    protected int NumPoints = 6;
+    /** Smoothing parameter for AP-RDF */
+    protected double SmoothParameter = 4.0;
+    /** List of elemental properties to use for weighting */
+    protected Set<String> ElementalProperties = new TreeSet<>();
 
     @Override
     public void setOptions(List<Object> Options) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double cutoff, smooth;
+        int nPoints;
+        
+        // Clear old properites
+        clearElementalProperties();
+        
+        // Parse options
+        try {
+            cutoff = Double.parseDouble(Options.get(0).toString());
+            nPoints = Integer.parseInt(Options.get(1).toString());
+            smooth = Double.parseDouble(Options.get(2).toString());
+            
+            for (Object obj : Options.subList(3, Options.size())) {
+                addElementalProperty(obj.toString());
+            }
+        } catch (Exception e) {
+            throw new Exception(printUsage());
+        }
+        
+        // Set options 
+        setCutoffDistance(cutoff);
+        setNumPoints(nPoints);
+        setSmoothParameter(smooth);
     }
 
     @Override
     public String printUsage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Usage: <cutoff> <# points> <smoothing> <properties...>";
+    }
+
+    /**
+     * Define cutoff distance for AP-RDF. 
+     * @param cutoff Desired cutoff
+     */
+    public void setCutoffDistance(double cutoff) {
+        this.CutoffDistance = cutoff;
+    }
+
+    /**
+     * Set number of distances to evaluated for AP-RDF
+     * @param numPoints Desired number of points
+     */
+    public void setNumPoints(int numPoints) {
+        this.NumPoints = numPoints;
+    }
+
+    /**
+     * Set smoothing parameter for RDF. Controls how s
+     * @param SmoothParameter 
+     */
+    public void setSmoothParameter(double SmoothParameter) {
+        this.SmoothParameter = SmoothParameter;
+    }
+    
+    /**
+     * Clear list of elemental properties.
+     */
+    public void clearElementalProperties() {
+        ElementalProperties.clear();
+    }
+    
+    /**
+     * Add elemental property to set of those used for generating attributes
+     * @param propertyName Property name
+     */
+    public void addElementalProperty(String propertyName) {
+        ElementalProperties.add(propertyName);
     }
 
     @Override
     public void addAttributes(Dataset data) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // Check whether dataset is a crystal-structure dataset
+        if (! (data instanceof CrystalStructureDataset)) {
+            throw new RuntimeException("Data must be a CrystalStructureDataset");
+        }
+        CrystalStructureDataset dataPtr = (CrystalStructureDataset) data;
+        
+        // Create tool to compute AP-RDF
+        APRDFAnalysis tool = new APRDFAnalysis();
+        tool.setCutoffDistance(CutoffDistance);
+        tool.setNWindows(NumPoints);
+        tool.setSmoothingFactor(SmoothParameter);
+        
+        // Create attribute names
+        List<String> names = new ArrayList<>(NumPoints * ElementalProperties.size());
+        double[] evalDist = tool.getEvaluationDistances();
+        for (String prop : ElementalProperties) {
+            for (double dist : evalDist) {
+                names.add(String.format("APRDF_%s_R=%f_B=%f", prop,
+                        dist, SmoothParameter));
+            }
+        }
+        data.addAttributes(names);
+        
+        // Loop through each entry, compute attributes
+        double[] newAttrs = new double[names.size()];
+        for (BaseEntry entry : data.getEntries()) {
+            // Get the structure
+            Cell strc = ((AtomicStructureEntry) entry).getStructure();
+            
+            // Prepare the APRDF tool
+            tool.analyzeStructre(strc);
+            
+            // Loop through each property
+            int pos = 0;
+            for (String prop : ElementalProperties) {
+                // Get the elemental properties for each atom type
+                double[] propLookup = dataPtr.getPropertyLookupTable(prop);
+                double[] atomProp = new double[strc.nTypes()];
+                
+                for (int t=0; t<atomProp.length; t++) {
+                    atomProp[t] = propLookup[ArrayUtils.indexOf(LookupData.ElementNames, strc.getTypeName(t))];
+                }
+                
+                // Compute the APRDF
+                double[] apRDF = tool.computeAPRDF(atomProp);
+                for (double ap : apRDF) {
+                    newAttrs[pos++] = ap;
+                }
+            }
+            
+            // Store the attributes
+            entry.addAttributes(newAttrs);
+        }
     }
 
     @Override
     public String printDescription(boolean htmlFormat) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String output = getClass().getName() + (htmlFormat ? " " : ": ");
+        
+        // Add number of properties
+        output += "(" + (NumPoints * ElementalProperties.size()) + ")";
+        
+        // Add description
+        output += " Computes the Atomic-Property-Weighted Radial Distribution "
+                + "Function (AP-RDF) for a material at " + NumPoints + " intervals ranging from ";
+        output += String.format("%.2f to %.2f Angstrom ", CutoffDistance / NumPoints, CutoffDistance);
+        output += String.format("with a smoothing factor of B=%.1f 1/Angstrom.", SmoothParameter);
+        
+        // Add properties
+        output += " The AP-RDF is weighted by " + ElementalProperties.size()
+                + " different elemental properties:\n";
+        if (htmlFormat) {
+            output += "<br>";
+        }
+        Iterator<String> iter = ElementalProperties.iterator();
+        output += iter.next();
+        while (iter.hasNext()) {
+            output += ", " + iter.next();
+        }
+        
+        return output;
     }
 
     @Override
@@ -60,8 +211,10 @@ public class APRDFAttributeGenerator extends BaseAttributeGenerator
                 new Citation(this.getClass(),
                         "Article", 
                         new String[]{"M. Fernandez", "et al."},
-                        "Rapid and Accurate Machine Learning Recognition of High Performing Metal Organic Frameworks for CO 2 Capture",
-                        "http://dx.doi.org/10.1021/jz501331m",
+                        "Atomic Property Weighted Radial Distribution Functions "
+                                + "Descriptors of Metal–Organic Frameworks for "
+                                + "the Prediction of Gas Uptake Capacity",
+                        "http://pubs.acs.org/doi/abs/10.1021/jp404287t",
                         null)
         ));
         
