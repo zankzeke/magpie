5efc5583b7247149ee9015f018e9ac5fd19dd8d0
Logan Ward <ward.logan.t@gmail.com>
2016-04-14 22:26:06 -0500

    Made lasso python script for Windows-friendly
    Also made a memory usage upgrade

--- py/lasso_attribute_selection.py
+++ py/lasso_attribute_selection.py
@@ -52,138 +52,140 @@ import numpy as np
 #  Date: 14 April 2016
 #
 
+if __name__ == '__main__':
+	# Default parameters
+	n_params_lasso = 16
+	corr_downselect = None
+	max_dimensionality = 5
+	final_model = LinearRegression
+	n_procs = 1
+	get_best = False 
+	cv = None
 
-# Default parameters
-n_params_lasso = 16
-corr_downselect = None
-max_dimensionality = 5
-final_model = LinearRegression
-n_procs = 1
-get_best = False 
-cv = None
-
-# Loop over argv
-pos = 1
-while pos < len(sys.argv):
-	cmd = sys.argv[pos].lower()
-	if cmd == "-n_lasso":
+	# Loop over argv
+	pos = 1
+	while pos < len(sys.argv):
+		cmd = sys.argv[pos].lower()
+		if cmd == "-n_lasso":
+			pos += 1
+			n_params_lasso = int(sys.argv[pos])
+		elif cmd == "-corr_downselect":
+			pos += 1
+			corr_downselect = int(sys.argv[pos])
+		elif cmd == "-max_dim":
+			pos += 1
+			max_dimensionality = int(sys.argv[pos])
+		elif cmd == "-n_procs":
+			pos += 1
+			n_procs = int(sys.argv[pos])
+		elif cmd == "-pick_best":
+			get_best = True
+		elif cmd == "-cv_method":
+			pos += 1
+			split_frac = float(sys.argv[pos])
+			if split_frac <= 0 or split_frac >= 1:
+				raise Exception('Split fraction must be between 0 and 1')
+			pos += 1
+			niter = int(sys.argv[pos])
+			cv = (split_frac, niter)
+		else:
+			raise Exception('command %s not recognized'%cmd)
 		pos += 1
-		n_params_lasso = int(sys.argv[pos])
-	elif cmd == "-corr_downselect":
-		pos += 1
-		corr_downselect = int(sys.argv[pos])
-	elif cmd == "-max_dim":
-		pos += 1
-		max_dimensionality = int(sys.argv[pos])
-	elif cmd == "-n_procs":
-		pos += 1
-		n_procs = int(sys.argv[pos])
-	elif cmd == "-pick_best":
-		get_best = True
-	elif cmd == "-cv_method":
-		pos += 1
-		split_frac = float(sys.argv[pos])
-		if split_frac <= 0 or split_frac >= 1:
-			raise Exception('Split fraction must be between 0 and 1')
-		pos += 1
-		niter = int(sys.argv[pos])
-		cv = (split_frac, niter)
-	else:
-		raise Exception('command %s not recognized'%cmd)
-	pos += 1
 
-# Raise a few warnings
-if cv is None and get_best:
-	print >>sys.stderr, "[Warning] You're asking this code to find the best attribute set size and not using cross-validation"
-	print >>sys.stderr, "[Warning]     Adding attributes will only make the fitness to trainng set get better for linear regression"
-	print >>sys.stderr, "[Warning]     Using CV by adding a \"-cv_method\" flag is *highly* recommended"
+	# Raise a few warnings
+	if cv is None and get_best:
+		print >>sys.stderr, "[Warning] You're asking this code to find the best attribute set size and not using cross-validation"
+		print >>sys.stderr, "[Warning]     Adding attributes will only make the fitness to trainng set get better for linear regression"
+		print >>sys.stderr, "[Warning]     Using CV by adding a \"-cv_method\" flag is *highly* recommended"
 
-# Get the data
-data = pd.read_csv(sys.stdin)
-X = data[data.columns[:-1]].as_matrix()
-y = data[data.columns[-1]].as_matrix()
-columns = data.columns
-del data # No longer needed in memory
+	# Get the data
+	data = pd.read_csv(sys.stdin)
+	X = data[data.columns[:-1]].as_matrix()
+	y = data[data.columns[-1]].as_matrix()
+	columns = data.columns
+	del data # No longer needed in memory
 
-# Scale the features
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X, y)
+	# Scale the features
+	scaler = MinMaxScaler()
+	X = scaler.fit_transform(X, y)
 
-# Sanity check: Make sure n_params_lasso is less
-#  than the total number of attributes
-n_params_lasso = min(n_params_lasso, len(columns) - 1)
+	# Sanity check: Make sure n_params_lasso is less
+	#  than the total number of attributes
+	n_params_lasso = min(n_params_lasso, len(columns) - 1)
 
-# Compute the first n_params_lasso from the lasso path
-alphas, coefs, dual_path  = lasso_path(X, y)
-for coef in coefs.T:
-	count = np.count_nonzero(coef)
-	if count >= n_params_lasso:
-		break
-if count < n_params_lasso:
-	# Increase manually
-	alpha = alphas[-1]
-	alpha_step = alpha / alphas[-2]
-	while count < n_params_lasso:
-		alpha = alpha * alpha_step
-		model = Lasso(alpha=alpha)
-		model.fit(X,y)
-		coef = model.coef_
+	# Compute the first n_params_lasso from the lasso path
+	alphas, coefs, dual_path  = lasso_path(X, y)
+	for coef in coefs.T:
 		count = np.count_nonzero(coef)
-		
-
-# Get the LASSO selected attributes
-attr_ids = list(np.nonzero(coef)[0])
-print "[Status] Selected %d attributes via LASSO: "%len(attr_ids), " ".join([ columns[x] for x in attr_ids ])
-
-# Optinal: Iteratively remove highly-correlated attributes
-if not corr_downselect is None:
-	while len(attr_ids) > corr_downselect:
-		# Get the attributes
-		X_sub = X[:,attr_ids]
-
-		# Compute squared correlation between everyone
-		corr = np.corrcoef(X_sub)
-		corr = np.power(corr, 2)
-
-		# Compute the average correlation for each attribute
-		attr_corr = [ (attr_ids[i], np.mean(corr[:,i]) + np.mean(corr[i,:])) for i in range(len(attr_ids)) ]
-
-		# Remove the highest
-		highest_attr = max(attr_corr, key=lambda x: x[1])[0]
-		attr_ids.remove(highest_attr)
-	print "[Status] Downselected to %d loosely-correlated attributes: "%len(attr_ids), " ".join([ columns[x] for x in attr_ids ])
-
-
-# Define method used to compute CV score
-#  A function to return MSE given a reduced dataset
-if cv is None:
-	def score(X):
-		model = final_model()
-		model.fit(X,y)
-		y_pred = model.predict(X)
-		return mean_squared_error(y, y_pred)
-else:
-	def score(X):
-		return -1 * np.mean(cross_val_score(final_model(), X, y, 'mean_squared_error', \
-			cv=ShuffleSplit(len(y), n_iter=cv[1], test_size=cv[0], random_state=1)))
-
-# Loop through all possible combinations
-best_score_of_all = float('inf')
-best_comb_of_all = None
-dim_range = range(1,max_dimensionality+1) if get_best else [max_dimensionality]
-for dim in dim_range:
-	def run_score(comb):
-		return (comb, score(X[:,comb]))
-	scores = Parallel(n_jobs=n_procs)([ \
-		delayed(run_score)(comb) \
-		for comb in itertools.combinations(attr_ids, dim) \
-	])
-	best_comb, best_score = min(scores, key=lambda x: x[1])
-	if best_score < best_score_of_all:
-		best_comb_of_all = best_comb
-		best_score_of_all = best_score
-		
-	print '[Status]', best_score, " ".join([columns[c] for c in best_comb])
+		if count >= n_params_lasso:
+			break
+	if count < n_params_lasso:
+		# Increase manually
+		alpha = alphas[-1]
+		alpha_step = alpha / alphas[-2]
+		while count < n_params_lasso:
+			alpha = alpha * alpha_step
+			model = Lasso(alpha=alpha)
+			model.fit(X,y)
+			coef = model.coef_
+			count = np.count_nonzero(coef)
 	
-# If user wanted the best choice
-print '[Answer]', " ".join([columns[c] for c in best_comb_of_all])
+
+	# Get the LASSO selected attributes
+	attr_ids = list(np.nonzero(coef)[0])
+	print "[Status] Selected %d attributes via LASSO: "%len(attr_ids), " ".join([ columns[x] for x in attr_ids ])
+
+	# Optinal: Iteratively remove highly-correlated attributes
+	if not corr_downselect is None:
+		while len(attr_ids) > corr_downselect:
+			# Get the attributes
+			X_sub = X[:,attr_ids]
+
+			# Compute squared correlation between everyone
+			corr = np.corrcoef(X_sub)
+			corr = np.power(corr, 2)
+
+			# Compute the average correlation for each attribute
+			attr_corr = [ (attr_ids[i], np.mean(corr[:,i]) + np.mean(corr[i,:])) for i in range(len(attr_ids)) ]
+
+			# Remove the highest
+			highest_attr = max(attr_corr, key=lambda x: x[1])[0]
+			attr_ids.remove(highest_attr)
+		print "[Status] Downselected to %d loosely-correlated attributes: "%len(attr_ids), " ".join([ columns[x] for x in attr_ids ])
+
+
+	# Define method used to compute CV score
+	#  A function to return MSE given a combination of attributes
+	if cv is None:
+		def score(comb):
+			model = final_model()
+			X_sub = X[:,comb]
+			model.fit(X_sub,y)
+			y_pred = model.predict(X_sub)
+			return mean_squared_error(y, y_pred)
+	else:
+		def score(comb):
+			return -1 * np.mean(cross_val_score(final_model(), X[:,comb], y, 'mean_squared_error', \
+				cv=ShuffleSplit(len(y), n_iter=cv[1], test_size=cv[0], random_state=1)))
+
+	# Loop through all possible combinations
+	best_score_of_all = float('inf')
+	best_comb_of_all = None
+	dim_range = range(1,max_dimensionality+1) if get_best else [max_dimensionality]
+	for dim in dim_range:
+		def run_score(comb):
+			return (comb, score(comb))
+		scores = Parallel(n_jobs=n_procs)([ \
+			delayed(run_score)(comb) \
+			for comb in itertools.combinations(attr_ids, dim) \
+		])
+		best_comb, best_score = min(scores, key=lambda x: x[1])
+		if best_score < best_score_of_all:
+			best_comb_of_all = best_comb
+			best_score_of_all = best_score
+	
+		print '[Status]', best_score, " ".join([columns[c] for c in best_comb])
+
+	# If user wanted the best choice
+	print '[Answer]', " ".join([columns[c] for c in best_comb_of_all])
+
