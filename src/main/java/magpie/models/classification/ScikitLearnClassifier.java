b45236cbccb4654a7bfccc4a67f6a3cb0eaef66f
Logan Ward <ward.logan.t@gmail.com>
2016-10-02 10:03:15 -0500

    Refactored ScikitClassifier

--- src/main/java/magpie/models/classification/ScikitLearnClassifier.java
+++ src/main/java/magpie/models/classification/ScikitLearnClassifier.java
@@ -6,8 +6,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterInputStream;
+import java.util.zip.InflaterOutputStream;
 import magpie.data.BaseEntry;
 import magpie.data.Dataset;
+import magpie.models.interfaces.ExternalModel;
+import magpie.models.utility.ExternalModelUtility;
+import magpie.utility.UtilityOperations;
 
 /**
  * Uses Scikit-learn to train a classification model. User must provide the path to
@@ -36,7 +42,7 @@ import magpie.data.Dataset;
  *
  * @author Logan Ward
  */
-public class ScikitLearnClassifier extends BaseClassifier {
+public class ScikitLearnClassifier extends BaseClassifier implements ExternalModel {
     /**
      * Scikit-learn model. This is just the the pickle file as a char array that
      * has been split into chunks
@@ -58,13 +64,23 @@ public class ScikitLearnClassifier extends BaseClassifier {
      * Compression level on ScikitLearn model
      */
     private int CompressionLevel = 5;
+    /** 
+     * Whether to print output from python stdout to screen
+     */
+    public boolean Debug = false;
 
     @Override
-    protected void finalize() throws Throwable {
-        ScikitServer.destroy();
-        super.finalize();
+    public void close() throws Exception {
+        closeServer();
     }
-
+    
+    @Override
+    public void closeServer() {
+        if (serverIsRunning()) {
+            ScikitServer.destroy();
+        }
+    }
+    
     @Override
     public ScikitLearnClassifier clone() {
         ScikitLearnClassifier x = (ScikitLearnClassifier) super.clone();
@@ -98,6 +114,11 @@ public class ScikitLearnClassifier extends BaseClassifier {
         return "Usage: <path to model.pkl>";
     }
 
+    @Override
+    public int getPort() {
+        return Port;
+    }
+
     /**
      * Define how well model is compressed after training.
      * 
@@ -110,6 +131,7 @@ public class ScikitLearnClassifier extends BaseClassifier {
      * @throws Exception 
      * @see #ScikitModel
      */
+    @Override
     public void setCompressionLevel(int level) throws Exception {
         if (level < 0 || level > 9) {
             throw new IllegalArgumentException("Compression level must be 0-9");
@@ -117,23 +139,23 @@ public class ScikitLearnClassifier extends BaseClassifier {
         this.CompressionLevel = level;
     }
     
-    
-
     /**
      * Read model from an input stream
      *
      * @param input Input stream providing model data
      * @throws java.io.IOException
      */
+    @Override
     public void readModel(InputStream input) throws Exception {
         // Clear out old model
         ScikitModel.clear();
         
-        // Create the byte reader
-        BufferedInputStream fp = new BufferedInputStream(input);
+        // Read in data from input stream, and iteratively compress it
+        DeflaterInputStream deflater = new DeflaterInputStream(input, 
+                new Deflater(CompressionLevel));
         while (true) {
             byte[] chunk = new byte[BufferSize];
-            int nRead = fp.read(chunk);
+            int nRead = deflater.read(chunk);
             if (nRead == chunk.length) {
                 ScikitModel.add(chunk);
             } else if (nRead > 0) {
@@ -145,292 +167,66 @@ public class ScikitLearnClassifier extends BaseClassifier {
     }
 
     /**
-     * Write stored model to disk.
+     * Write model to output stream
      *
-     * @param path Path to desired output file
+     * @param output
      * @throws java.io.IOException
      */
-    public void writeModel(String path) throws IOException {
-        BufferedOutputStream fo = new BufferedOutputStream(new FileOutputStream(path));
+    @Override
+    public void writeModel(OutputStream output) throws IOException {
+        // Create the inflater
+        InflaterOutputStream inflater = new InflaterOutputStream(output);
+        
+        // For each chunk, uncompress it
         for (byte[] chunk : ScikitModel) {
-            fo.write(chunk);
+            inflater.write(chunk);
         }
-        fo.close();
     }
 
-    /**
-     * Start the server hosting the Scikit model. This process will initialize
-     * the server and get the communication port number.
-     *
-     * @throws java.lang.Exception
-     */
-    protected void startScikitServer() throws Exception {
-        String script = "import pickle\n"
-                + "from sys import argv\n"
-                + "import socket\n"
-                + "import array\n"
-                + "import gzip\n"
-                + "import binascii\n"
-                + "from sys import stdout\n"
-                + "\n" 
-                + "startPort = 5482; # First port to check\n"
-                + "endPort = 5582; # Last port to check\n"
-                + "try:\n"
-                + " fp = gzip.open(argv[1], 'rb')\n"
-                + " model = pickle.load(fp)\n"
-                + " fp.close()\n"
-                + "except:\n"
-                + " fp = open(argv[1], 'rb')\n"
-                + " model = pickle.load(fp)\n"
-                + " fp.close()\n"
-                + "\n"
-                + "port = startPort;\n"
-                + "ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)\n"
-                + "while port <= endPort:\n"
-                + "	try:\n"
-                + "		ss.bind(('localhost', port))\n"
-                + "	except:\n"
-                + "		port = port + 1\n"
-                + "		continue\n"
-                + "	break\n"
-                + "ss.listen(0) # Only allow one connection\n"
-                + "print \"Listening on port\", port\n"
-                + "stdout.flush();\n"
-                + "\n"
-                + "def trainModel(fi, fo):\n"
-                + "	nRows = int(fi.readline())\n"
-                + "	X = []\n"
-                + "	y = []\n"
-                + "	for i in range(nRows):\n"
-                + "		line = fi.readline()\n"
-                + "		x = array.array('d')\n"
-                + "		temp = [ float(w) for w in line.split() ]\n"
-                + "		x.fromlist(temp[1:])\n"
-                + "		X.append(x)\n"
-                + "		y.append(temp[0])\n"
-                + "	model.fit(X,y)\n"
-                + "	\n"
-				+ "	gzo = gzip.GzipFile(fileobj = fo, mode='wb', compresslevel=" + CompressionLevel + " )\n"
-                + "	pickle.dump(model, gzo)\n"
-                + "\n"
-                + "def runModel(fi, fo):\n"
-                + "	\n"
-                + "	# Recieve\n"
-                + "	nRows = int(fi.readline())\n"
-                + "	X = []\n"
-                + "	y = []\n"
-                + "	for i in range(nRows):\n"
-                + "		line = fi.readline()\n"
-                + "		x = array.array('d')\n"
-                + "		temp = [ float(w) for w in line.split() ]\n"
-                + "		x.fromlist(temp)\n"
-                + "		X.append(x)\n"
-                + "	\n"
-                + "	# Compute\n"
-                + "	y = model.predict_proba(X)\n"
-                + "	\n"
-                + "	# Send back\n"
-                + "	for yi in y:\n"
-                + "		print >>fo, ' '.join([ str(c) for c in yi])\n"
-                + "\n"
-                + "while 1:\n"
-                + " (client, address) = ss.accept()\n"
-                + " \n"
-                + " fi = client.makefile('r')\n"
-                + " fo = client.makefile('w')\n"
-                + " command = fi.readline()\n"
-                + " if \"train\" in command:\n"
-                + "     trainModel(fi, fo)\n"
-                + " elif \"run\" in command:\n"
-                + "     runModel(fi, fo)\n"
-                + " elif \"type\" in command:\n" 
-                + "     print >>fo, model\n"
-                + " fi.close()\n"
-                + " fo.close()\n"
-                + "		\n"
-                + "	# Close the client\n"
-                + " client.close()";
-        
-        // Write out the script file
-        File scriptFile = File.createTempFile("scikit", "py");
-        scriptFile.deleteOnExit();
-        PrintWriter fp = new PrintWriter(scriptFile);
-        fp.println(script);
-        fp.close();
-        
-        // Write out the model
-        File modelFile = File.createTempFile("scikit", "pkl");
-        modelFile.deleteOnExit();
-        writeModel(modelFile.getCanonicalPath());
+    @Override
+    public void startServer() throws Exception {
+        // Find the server code
+        File scriptFile = UtilityOperations.findFile("py/scikit_server.py");
         
         // Start the process
         ScikitServer = new ProcessBuilder("python", 
-                scriptFile.getCanonicalPath(), 
-                modelFile.getCanonicalPath()).start();
+                scriptFile.getCanonicalPath())
+                .start();
         
-        // Get the port number
-        BufferedReader fo = new BufferedReader(
-                new InputStreamReader(ScikitServer.getInputStream()));
-        String[] words = fo.readLine().split(" ");
-        Port = Integer.parseInt(words[3]);
-        
-        // Check if file is running
-        if (! serverIsRunning()) {
-            BufferedReader fe = new BufferedReader(
-                    new InputStreamReader(ScikitServer.getErrorStream()));
-            String line = fe.readLine();
-            while (line != null) {
-                System.err.println(line);
-                line = fe.readLine();
-            }
-            throw new IllegalArgumentException("Server failed to start");
-        }
-        
-        // Make sure the process is killed
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ScikitServer.destroy();
-            }
-        }));
-    }
-
-    /**
-     * Check if the python process is still running
-     *
-     * @return Whether it is still running
-     */
-    private boolean serverIsRunning() {
-        if (ScikitServer == null) {
-            return false;
-        }
-        try {
-            ScikitServer.exitValue();
-            return false;
-        } catch (Exception e) {
-            return true;
-        }
+        // Launch the server
+        Port = ExternalModelUtility.initializeServer(this, Debug);
     }
 
     @Override
-    protected void train_protected(Dataset TrainData) {
-        PrintWriter fo = null;
-        Socket socket = null;
-        
-        try {
-            if (! serverIsRunning()) {
-                startScikitServer();
-            }
-            
-            // Connect to server
-            socket = new Socket("localhost", Port);
-            
-            // Write to the socket that we want to train
-            fo = new PrintWriter(socket.getOutputStream());
-            fo.println("train");
-            
-            // Write out the data
-            fo.println(TrainData.NEntries());
-            for (BaseEntry e : TrainData.getEntries()) {
-                fo.print(e.getMeasuredClass());
-                for (int a=0; a<TrainData.NAttributes(); a++) {
-                    fo.print(" ");
-                    fo.print(e.getAttribute(a));
-                }
-                fo.println();
-            }
-            fo.flush();
-            
-            // Recieve the model
-            readModel(socket.getInputStream());
+    public boolean serverIsRunning() {
+        Process process = getProcess();
+        return ExternalModelUtility.isRunning(process);
+    }
 
-            // Check if server is still running
-            if (! serverIsRunning()) {
-                throw new IllegalArgumentException();
-            }
-            
-            socket.close();
+    @Override
+    public Process getProcess() {
+        return ScikitServer;
+    }
+
+    @Override
+    protected void train_protected(Dataset TrainData) {        
+        try {
+            // Connect to server and call the subprocess
+            ExternalModelUtility.trainModel(this, TrainData);
         } catch (Exception ex) {
-            // Read python error out
-            try {
-                System.err.println("Python Error Output:");
-                BufferedReader er = new BufferedReader(
-                        new InputStreamReader(ScikitServer.getErrorStream()));
-                String line = er.readLine();
-                while (line != null) {
-                    System.err.println("\t" + line);
-                    line = er.readLine();
-                }
-            } catch (Exception e) {
-            }
-            throw new Error(ex);
+            throw new RuntimeException(ex);
         } 
     }
-
+    
     @Override
-    public void run_protected(Dataset TrainData) {
-        PrintWriter fo = null;
-        Socket socket = null;
-        
+    public void run_protected(Dataset runData) {
         try {
-            if (! serverIsRunning()) {
-                startScikitServer();
-            }
-            
-            // Connect to server
-            socket = new Socket("localhost", Port);
-            
-            // Write to the socket that we want to run
-            fo = new PrintWriter(socket.getOutputStream());
-            fo.println("run");
-            
-            // Write out the data
-            fo.println(TrainData.NEntries());
-            for (BaseEntry e : TrainData.getEntries()) {
-                for (int a=0; a<TrainData.NAttributes(); a++) {
-                    fo.print(" ");
-                    fo.print(e.getAttribute(a));
-                }
-                fo.println();
-            }
-            fo.flush();
-            
-            // Recieve the results
-            BufferedReader fi = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            for (BaseEntry e : TrainData.getEntries()) {
-                double x;
-                try {
-                    String[] probStrs = fi.readLine().split(" ");
-                    double[] probs = new double[probStrs.length];
-                    for (int c=0; c<probStrs.length; c++) {
-                        probs[c] = Double.parseDouble(probStrs[c]);
-                    }
-                    e.setClassProbabilities(probs);
-                } catch (NumberFormatException ex) {
-                    // Skip
-                }
-            }
-            
-            // Done!
-            socket.close();
+            ExternalModelUtility.runModel(this, runData);
         } catch (Exception ex) {
-            try {
-                System.err.println("Python Error Output:");
-                BufferedReader er = new BufferedReader(
-                        new InputStreamReader(ScikitServer.getErrorStream()));
-                String line = er.readLine();
-                while (line != null) {
-                    System.err.println("\t" + line);
-                    line = er.readLine();
-                }
-            } catch (Exception e) {
-            }
-            throw new Error(ex);
-        } finally {
-            fo.close();
+            throw new RuntimeException(ex);
         }
     }
+
 
     @Override
     protected String printModel_protected() {
@@ -439,41 +235,12 @@ public class ScikitLearnClassifier extends BaseClassifier {
 
     @Override
     public List<String> printModelDescriptionDetails(boolean htmlFormat) {
-        if (! serverIsRunning()) {
-            try {
-                startScikitServer();
-            } catch (Exception e) {
-                throw new Error(e);
-            }
-        }
-        
         // Get the output
         List<String> output = super.printModelDescriptionDetails(htmlFormat);
         
-        // Open up socket to server
-        try {
-            Socket socket = new Socket("localhost", Port);
-            
-            // Indicate that we want type info
-            PrintWriter fo = new PrintWriter(socket.getOutputStream());
-            fo.println("type");
-            fo.flush();
-            
-            // Get the model info
-            BufferedReader fi = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            String line = fi.readLine();
-            while (line != null) {
-                output.add(line);
-                line = fi.readLine();
-            }
-            
-            // Close the socket
-            socket.close();
-        } catch (Exception e) {
-            throw new Error(e);
-        }
+        List<String> linesToAdd = ExternalModelUtility.getModelDescription(this);
         
+        output.addAll(linesToAdd);
         return output;
     }
 }
